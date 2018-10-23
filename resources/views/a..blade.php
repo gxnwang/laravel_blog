de19271b4e7b9e42eb1d2468a3264d7540e08e6b
Gao Xiaonao <shelizi2011@163.com>
2018-10-23 20:40:35 +0800

    first commit

---
+++ resources/views/a..blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+</head>
+<body>
+<div class="alert alert-warning" role="alert">
+    <strong>Warning!</strong>
+</div>
+</body>
+</html>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">首页</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+
+                <a class="btn btn-primary my-2 my-sm-0 mr-2" >注册</a>
+                <a class="btn btn-success my-2 my-sm-0" >登录</a>
+            </form>
+        </div>
+    </nav>
+</div>
